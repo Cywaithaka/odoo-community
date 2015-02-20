3c228449c43e2050a27c63806ed2bd1b6ce77393
Md. Matiar Rahman <matiar.rahman@gmail.com>
2015-02-20 19:36:30 +0600

    res_partner_skype

---
+++ res_partner_skype/__openerp__.py
@@ -0,0 +1,26 @@
+{
+    "name" : "Add skype field to partner info",
+    "version" : "0.1",
+    "author" : "Ivan Yelizariev",
+    "category" : "Base",
+    "website" : "https://yelizariev.github.io",
+    "description": """
+* Add skype field to partner info.
+* Add widget skype
+
+    <field name="skype" widget="skype" options="{'type':'call', 'video':0}"/>
+	 
+    """,
+    "depends" : ['web'],
+    "data":[
+        'views.xml',
+        'data.xml',
+        ],
+	 "js":[
+		  'static/src/js/skype.js'
+		  ],
+    "qweb" : [
+        'static/src/xml/base.xml',
+    ],
+    "installable": True
+}
